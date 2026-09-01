0ded538949e2517bcccde771e91469d8e37d298c
fadly-razak <fadlyproduktif@gmail.com>
2026-09-01 23:34:14 +0700

    feat: menambahkan grafik (belum sempurna tapi udah ada data)

--- resources/js/devisi-dashboard.js
+++ resources/js/devisi-dashboard.js
@@ -1,31 +1,35 @@
-import { Chart } from 'chart.js/auto';
-console.log('chart js berahasil di import');
+import { Chart } from "chart.js/auto";
+console.log("chart js berahasil di import");
 
-const canvas = document.getElementById('laporanChart');
+const canvas = document.getElementById("laporanChart");
 console.log(window.dataGrafik);
-console.log(canvas);
+// console.log(canvas);
 
-const labels = window.dataGrafik.map(item => item.kategori);
-const values = window.dataGrafik.map(item => item.total);
+const labels = window.dataGrafik.map((item) => item.kategori);
+const values = window.dataGrafik.map((item) => item.total);
 
 new Chart(canvas, {
-    type: 'Bar',
+    type: "bar",
     data: {
-        labels : labels,
-        datasets : [
-        {
-            label:'jumlah data',
-            data: values
-        }
-        ]
+        labels: labels,
+        datasets: [
+            {
+                label: "jumlah data",
+                data: values,
+            },
+        ],
     },
-    options:{
+    options: {
         responsive: true,
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
         scales: {
             y: {
                 beginAtZero: true,
-            }
-        }
-    }
-
+            },
+        },
+    },
 });
